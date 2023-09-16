1949fc9d16aa50d1c7f5bcf1cccdb4ac51a044c9
_hua <chhinhua@gmail.com>
2023-09-16 13:29:48 +0700

    complete create new Document, get a single Document

--- src/main/java/com/springbootmongodbtutorial/controller/TodoController.java
+++ src/main/java/com/springbootmongodbtutorial/controller/TodoController.java
@@ -4,11 +4,11 @@ import com.springbootmongodbtutorial.model.TodoDTO;
 import com.springbootmongodbtutorial.repository.TodoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1")
@@ -26,5 +26,25 @@ public class TodoController {
         if (todos.size() > 0) {
             return new ResponseEntity<List<TodoDTO>>(todos, HttpStatus.OK);
         } else return new ResponseEntity<>("No todos available", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/todos")
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO todoDTO) {
+        try {
+            todoDTO.setCreateAt(new Date(System.currentTimeMillis()));
+            todoRepository.save(todoDTO);
+            return new ResponseEntity<>(todoDTO, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<?> getTodo(@PathVariable String id) {
+        Optional<TodoDTO> todoDTO = todoRepository.findById(id);
+        if (todoDTO.isPresent()) {
+            return new ResponseEntity<>(todoDTO, HttpStatus.OK);
+        } else
+            return new ResponseEntity<>("Todo not found with id: " + id, HttpStatus.NOT_FOUND);
     }
 }
